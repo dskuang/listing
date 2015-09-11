2f6b451eb955872bdc2d445cbde9601edf9b6f59
dominic_kuang <dskuang@gmail.com>
2015-09-11 10:45:28 -0700

    added maps

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,38 +1,42 @@
 require 'rgeo/geo_json'
+require "yaml"
 
 class ListsController < ApplicationController
   def index
-    if params[:street].present?
-      @lists = List.where(street: params[:street])
+    if params[:list].present?
+      @lists = List.where(bedrooms: params[:list][:min_bed]..params[:list][:max_bed]).to_json
+
     else
       @lists = List.all
+      @list = List.find(1)
+      if @list
+        newlist = @list.attributes
+        newlist.delete("lat")
+        newlist.delete("lng")
+        newlist.delete("updated_at")
+        newlist.delete("created_at")
+        newlist["marker-size"] = "large"
+        newlist["marker-color"] = "#fc4353"
+        newlist["marker-symbol"] ="monument"
+        feature = {}
+        feature["type"] = "Feature"
+        feature["geometry"] = {"type" => "Point", "coordinates" => [@list.lat, @list.lng]}
+        feature["properties"] = newlist
 
+        @allfeature = {}
+        @allfeature["type"] = "FeatureCollection"
+        @allfeature["features"] = [feature]
+        respond_to do |format|
+          format.html
+          format.json { render json: @allfeature }  # respond with the created JSON object
+        end
+      end
     end
-     @list = List.find(1)
-     newlist = @list.to_json
-     newlist.delete("lat")
-     newlist.delete("lng")
-     feature = {}
-     feature["type"] = "feature"
-    #  feature["geometry"] = {"type": "Point"}
-     feature["properties"] = newlist
-
-     @allfeature = {}
-     @allfeature["type"] = "FeatureCollection"
-     @allfeature["features"] = [feature]
-    # @list = "#{@list}"
-    @allfeature = RGeo::GeoJSON.decode(@allfeature, :json_parse => :json)
-    @allfeature.as_text
-    # str1 = '{"type":"Point","coordinates":[1,2]}'
-    # @list = RGeo::GeoJSON.decode(str1, :json_parser => :json)
-
   end
 
   def import
     List.import(params[:file])
     redirect_to root_url, notice: "Lists imported."
   end
-
-
 
 end
